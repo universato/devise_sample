7eaa7560536115d7c02831e796788415c11a888f
universato <universato@gmail.com>
2021-07-13 00:51:48 +0900

    Add authenticate_user!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  # authenticate_user!
+  # ユーザーがログインしているかどうか確かめ、未ログインの場合はログイン画面へリダイレクトする。
+  # https://github.com/heartcombo/devise/blob/5d5636f03ac19e8188d99c044d4b5e90124313af/lib/devise/controllers/helpers.rb#L116-L119
+  before_action :authenticate_user!
 end
